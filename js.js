95f37f69d8ad8094ea35a1cf2d7348d3543ee99c
SaxonJude <67439444+SaxonJude@users.noreply.github.com>
2020-08-29 11:33:25 +0100

    Create js.js

---
+++ js.js
@@ -0,0 +1,27 @@
+var inputVal = document.querySelector('.amountInput');
+var button = document.querySelector('.button');
+var output = document.querySelector('.output');
+
+button.addEventListener('click', calcTip);
+inputVal.addEventListener('keypress', (e) => {
+  if(e.key === 'Enter'){
+    calcTip();
+  }
+});
+
+function calcTip() {
+  var bill = inputVal.value;
+  var tip;
+
+  if(bill < 50) {
+    tip = 0.25;
+  } else if (bill >= 50 && bill < 100) {
+    tip = 0.2;
+  } else if (bill >= 100 && bill < 200) {
+    tip = 0.15;
+  } else {
+    tip = 0.1;
+  };
+  var total = bill * tip;
+  output.textContent = `You should tip $${total} for $${bill}`;
+};
